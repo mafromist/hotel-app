d4cf1ad3810d9508879f46b4b5f7cb48df1f0f86
MugeMeggy <mafromist@gmail.com>
2021-02-16 18:04:21 +0000

    star review has changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   state = {
     houses : [
       {id: 1, houseimg: house1, star: 5, guestImg: guest1, guestName: "Cherly", guestFrom:"United States", review:"Gary was such a great host during our time in Brighton. He made us feel at home and was helpful with any questions we had about directions or things to do. The room he has..."},
-      {id: 2, houseimg: house2, star: 2, guestImg: guest2, guestName: "Charlie", guestFrom:"Gana", review: "Very accommodating with my booking and welcoming to all of us. Thanks!"},
+      {id: 2, houseimg: house2, star: 4, guestImg: guest2, guestName: "Charlie", guestFrom:"Gana", review: "Very accommodating with my booking and welcoming to all of us. Thanks!"},
       {id: 3, houseimg: house3, star: 3, guestImg: guest3, guestName: "John Wayne", guestFrom:"Canada", review: "Wow... Alyson is turly an amazing person.1st class accommodation and we got the difficult news that a loved has passed the morning we were schedule to leave. Alyson.."}
     ]
   }
